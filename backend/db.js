0c6e4fd79d6c437e621dec2517dd4aadf86d1aee
Red De Guzman <redefinered@gmail.com>
2025-02-09 22:15:36 +0800

    fix: syntax error

--- backend/db.js
+++ backend/db.js
@@ -4,5 +4,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = new pg.Pool({
-  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 });
