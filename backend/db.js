dafdfdbe504d05533b31afe863da5fd9a14dc6c8
Red De Guzman <redefinered@gmail.com>
2025-02-09 22:11:16 +0800

    feat: simplify pg connection

--- backend/db.js
+++ backend/db.js
@@ -4,9 +4,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = new pg.Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT || 5432,
+  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 });
