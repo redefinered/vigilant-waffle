a50b950d3dd240eab79da1723ae1dceb0e109bfc
Red De Guzman <redefinered@gmail.com>
2025-02-09 22:57:36 +0800

    feat: add more test

--- frontend/src/__tests__/App.test.jsx
+++ frontend/src/__tests__/App.test.jsx
@@ -1,13 +1,36 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 
-import React from "react"
+import React from "react";
 import App from "../App";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 it("renders Task List heading", () => {
   render(<App />);
   const heading = screen.getByText(/Task List/i);
   expect(heading).toBeInTheDocument();
+});
+
+it("renders input fields", () => {
+  render(<App />);
+  const nameInput = screen.getByPlaceholderText(/Task Name/i);
+  const descInput = screen.getByPlaceholderText(/Task Description/i);
+  
+  expect(nameInput).toBeInTheDocument();
+  expect(descInput).toBeInTheDocument();
+});
+
+it("renders an 'Add Task' button", () => {
+  render(<App />);
+  const button = screen.getByRole("button", { name: /Add Task/i });
+  expect(button).toBeInTheDocument();
+});
+
+it("allows users to type in the input field", () => {
+  render(<App />);
+  const nameInput = screen.getByPlaceholderText(/Task Name/i);
+  
+  fireEvent.change(nameInput, { target: { value: "New Task" } });
+  expect(nameInput.value).toBe("New Task");
 });
